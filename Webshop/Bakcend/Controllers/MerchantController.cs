da75f1b4e5841c060cbc8c9d98cbec54c9edad65
thurzob <thurzob@kkszki.hu.com>
2024-04-21 16:09:38 +0200

    Vizsgára

---
+++ Webshop/Bakcend/Controllers/MerchantController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Bakcend.Models;
+using Bakcend.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+
+namespace Bakcend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MerchantController : ControllerBase
+    {
+
+        
+    }
+}
---
+++ Webshop/Bakcend/Models/DTO/Dtos.cs
@@ -0,0 +1,9 @@
+﻿namespace Bakcend.Models.DTO
+{
+    public record CreateUserDto(string Name, string Password, string Email, int MerchantId);
+    public record UpdateUserDto(string Name, string Password, string Email);
+    public record CreateQuantityDto(int Extant, int Sold, int Revenue);
+    public record UpdateQuantityDto(int Extant, int Sold, int Revenue);
+
+
+}
